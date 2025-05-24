f969f62feed3b858c7343ae4cb3ce01e6a65de04
supriyaacharyaa <supriya.acharya63@gmail.com>
2025-05-24 11:23:14 +0545

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,6 +5,7 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
+export const currency = '$'
 export const assets ={
     logo,
     add_icon,
@@ -14,4 +15,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'http://localhost:4000'
+export const url = 'https://food-del-backend-ndnm.onrender.com'
